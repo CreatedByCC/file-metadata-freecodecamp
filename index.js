76abb6295a559fe1085883a1dcb6942dcd7cc026
CC <chantelh88@gmail.com>
2024-12-11 15:22:29 +1300

    Installed multer package

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
-var express = require('express');
-var cors = require('cors');
-require('dotenv').config()
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+const multer = require('multer');
+const path = require('path');
 
-var app = express();
+const app = express();
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
@@ -10,6 +12,17 @@ app.use('/public', express.static(process.cwd() + '/public'));
 app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
+
+// Configure Multer 
+const storage = multer.diskStorage({ 
+  destination: (req, file, cb) => { 
+    cb(null, 'uploads/'); 
+  }, filename: (req, file, cb) => { 
+    cb(null, Date.now() + path.extname(file.originalname)); 
+  } 
+});
+
+const upload = multer({ storage: storage });
 
 
 
